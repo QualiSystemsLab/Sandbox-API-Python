a6c162fa8a2ec231d64924776668a514dacc7fc6
katzy687 <katzy687@gmail.com>
2021-11-27 00:14:51 +0200

    updated docs

--- setup.py
+++ setup.py
@@ -18,14 +18,15 @@ def lines_from_file(file_name: str) -> List[str]:
 setup(
     name="cloudshell_sandboxapi_wrapper",
     description="Python client for CloudShell Sandbox REST api - consume sandboxes via CI",
-    keywords=["cloudshell", "sandbox", "api", "CI"],
+    keywords=["cloudshell", "sandbox", "api", "rest", "CI"],
     url="https://github.com/QualiSystemsLab/Sandbox-API-Python",
     author="sadanand.s",
     author_email="sadanand.s@quali.com",
     license="Apache 2.0",
     packages=find_packages(),
     version=read_file("version.txt"),
-    long_description=read_file("README.rst"),
+    long_description=read_file("README.MD"),
+    long_description_content_type='text/markdown',
     install_requires=lines_from_file("requirements.txt"),
     test_requires=lines_from_file("test-requirements.txt"),
     classifiers=[
